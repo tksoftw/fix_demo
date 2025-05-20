4ecea10d1ea0bb2244a9fdea5c9c98a73d00d158
tk <62570383+tksoftw@users.noreply.github.com>
2025-05-20 00:29:01 -0400

    remove unused file & old code

--- include/BasicServer.hpp
+++ include/BasicServer.hpp
@@ -11,18 +11,6 @@
 
 namespace F44 = FIX8::FIX44;
 
-class ConcreteSession : public FIX8::Session {
-    F44::FIX44_Router router;
-public:
-    ConcreteSession(const FIX8::F8MetaCntx& ctx, const FIX8::sender_comp_id& sci, FIX8::Persister *persist=nullptr,
-        FIX8::Logger *logger=nullptr, FIX8::Logger *plogger=nullptr) 
-        : FIX8::Session(ctx, sci, persist, logger, plogger) {}
-    
-    bool handle_application(const unsigned seqnum, const FIX8::Message *&msg) override {
-        return msg->process(router);
-    }
-};
-
 class BasicServer {
     std::unique_ptr<FIX8::ServerSession<FIX44Session>> server;
     std::unique_ptr<FIX8::SessionInstanceBase> session;
